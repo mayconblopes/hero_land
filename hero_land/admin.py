f2c276c500fb895605cd919da95938485b576955
mayconblopes <lopes.maycon@gmail.com>
2023-06-02 00:32:00 -0300

    refactor(admin.py): added readonly fields

--- hero_land/admin.py
+++ hero_land/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import HeroModel, CustomUserModel
 
 # Register your models here.
-admin.site.register(HeroModel)
 admin.site.register(CustomUserModel)
+@admin.register(HeroModel)
+class HeroAdmin(admin.ModelAdmin):
+    readonly_fields = ['cover_id', 'cover_url']
