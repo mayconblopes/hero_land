0c1a16da9ede229440220fb3ceedfab10a70c562
mayconblopes <lopes.maycon@gmail.com>
2023-06-02 00:33:06 -0300

    feat(models.py): upload cover image to imagekitio

--- hero_land/models.py
+++ hero_land/models.py
@@ -1,9 +1,58 @@
+import shutil
 import uuid
 
 from django.db import models
 from django.contrib.auth.models import User
 from markdownfield.models import MarkdownField, RenderedMarkdownField
+from imagekitio import ImageKit
 
+
+# Method to save files on imagekitio
+def imagekitio_save(super, file_field, file_name, cover_url, cover_id, *args, **kwargs):
+    """overrides the instance.save method, uploading media file to imagekitio and retreaving it's URL"""
+
+    imagekit = ImageKit(
+
+        private_key='private_yr6ug8X/MYYWPneTsgT6Cheq1gY=',
+
+        public_key='public_qHQWN0tCwehMofMzXpS3b3XUE38=',
+
+        url_endpoint='https://ik.imagekit.io/mbl/heroland'
+
+    )
+
+    # update the database saving file to temp media folder on WebServer (will be deleted soon...)
+    super.save(*args, **kwargs)
+
+    # try/except to ignore errors in case of saving the model without uploading new image
+    try:
+        result = imagekit.upload_file(file=open(file_field.path, "rb"), file_name=file_name)
+        print('uploading image to imagekitio', result)
+        # if old cover exists on imagekitio, delete it before uploading a new cover
+        if cover_id:
+            print('old cover exists... preparing to delete it')
+            print(imagekit.delete_file(file_id=cover_id))
+
+        # get url and id of the new cover file
+        cover_url = result['response']['url']
+        cover_id = result['response']['fileId']
+    except FileNotFoundError:
+        cover_url = cover_url
+        cover_id = cover_id
+        print('Valid cover not uploaded', FileNotFoundError)
+
+
+# update the database saving the files's URL
+# super.save(*args, **kwargs)
+
+# delete temp media folder on WebServer
+# try/except to ignore errors in case of saving the model without uploading new image
+    try:
+        shutil.rmtree('media')
+    except FileNotFoundError:
+        pass
+
+    return cover_url, cover_id
 
 # Create your models here.
 
@@ -25,6 +74,14 @@ class HeroModel(models.Model):
     phone = models.CharField(max_length=30, default='')
     address = models.TextField(max_length=500, default='', blank=True, null=True)
     whatsapp = models.URLField(default='https://wa.me/55...', blank=True, null=True)
+    cover = models.ImageField(upload_to='hero-covers', default='')
+    cover_url = models.URLField(blank=True, null=True)
+    cover_id = models.CharField(blank=True, null=True, default='', max_length=50)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.cover_url, self.cover_id = imagekitio_save(super=super(), file_field=self.cover, file_name=self.name,
+                                                        cover_url=self.cover_url, cover_id=self.cover_id)
+        super().save()
