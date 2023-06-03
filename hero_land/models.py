9c67faf338ee89524eb9a1253d90eb19ada8369f
mayconblopes <lopes.maycon@gmail.com>
2023-06-02 21:54:27 -0300

    fix(model.py): added username field and linked Hero with User by ForeignKey

--- hero_land/models.py
+++ hero_land/models.py
@@ -5,6 +5,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from markdownfield.models import MarkdownField, RenderedMarkdownField
 from imagekitio import ImageKit
+from decouple import config
+
+PRIVATEKEY = config("PRIVATEKEY")
+PUBLICKEY = config("PUBLICKEY")
+ENDPOINT = config("ENDPOINT")
+
+print('private', PRIVATEKEY)
+print('public', PUBLICKEY)
 
 
 # Method to save files on imagekitio
@@ -12,16 +20,9 @@ def imagekitio_save(super, file_field, file_name, cover_url, cover_id, *args, **
     """overrides the instance.save method, uploading media file to imagekitio and retreaving it's URL"""
 
     imagekit = ImageKit(
-
-        #TODO (env)
-        private_key='TODO',
-
-        #TODO (env)
-        public_key='TODO',
-
-        #TODO (env)
-        url_endpoint='TODO'
-
+        private_key=PRIVATEKEY,
+        public_key=PUBLICKEY,
+        url_endpoint=ENDPOINT,
     )
 
     # update the database saving file to temp media folder on WebServer (will be deleted soon...)
@@ -44,12 +45,15 @@ def imagekitio_save(super, file_field, file_name, cover_url, cover_id, *args, **
         cover_id = cover_id
         print('Valid cover not uploaded', FileNotFoundError)
 
+    except ValueError:
+        print('Valid cover not uploaded', ValueError)
 
-# update the database saving the files's URL
-# super.save(*args, **kwargs)
 
-# delete temp media folder on WebServer
-# try/except to ignore errors in case of saving the model without uploading new image
+    # update the database saving the files's URL
+    # super.save(*args, **kwargs)
+
+    # delete temp media folder on WebServer
+    # try/except to ignore errors in case of saving the model without uploading new image
     try:
         shutil.rmtree('media')
     except FileNotFoundError:
@@ -57,17 +61,20 @@ def imagekitio_save(super, file_field, file_name, cover_url, cover_id, *args, **
 
     return cover_url, cover_id
 
+
 # Create your models here.
 
-class CustomUserModel(User):
-    objects = models.Manager()
-    hero = models.ForeignKey("HeroModel", on_delete=models.CASCADE)
+# class CustomUserModel(User):
+#     objects = models.Manager()
+#     hero = models.ForeignKey("HeroModel", on_delete=models.CASCADE)
 
 
 class HeroModel(models.Model):
     objects = models.Manager()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=150, blank=False, null=False)
     public_id = models.CharField(max_length=300, default=uuid.uuid4())
+    username = models.CharField(max_length=30, unique=True)
     skills = models.CharField(max_length=1000)
     bio = models.TextField(max_length=5000)
     instagram = models.URLField(default='', blank=True, null=True)
@@ -76,13 +83,14 @@ class HeroModel(models.Model):
     facebook = models.URLField(default='', blank=True, null=True)
     phone = models.CharField(max_length=30, default='')
     address = models.TextField(max_length=500, default='', blank=True, null=True)
-    whatsapp = models.URLField(default='https://wa.me/55...', blank=True, null=True)
-    cover = models.ImageField(upload_to='hero-covers', default='')
+    whatsapp = models.URLField(default='https://api.whatsapp.com/send/?phone=55...', blank=True, null=True)
+    cover = models.ImageField(upload_to='hero-covers', default='', blank=True, null=True)
     cover_url = models.URLField(blank=True, null=True)
     cover_id = models.CharField(blank=True, null=True, default='', max_length=50)
 
+
     def __str__(self):
-        return self.name
+        return self.username
 
     def save(self, *args, **kwargs):
         self.cover_url, self.cover_id = imagekitio_save(super=super(), file_field=self.cover, file_name=self.name,
