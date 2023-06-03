3ff1375f0552870b3a475da499362b2417b1bdbd
mayconblopes <lopes.maycon@gmail.com>
2023-06-02 22:08:38 -0300

    * feat(LandingPage.tsx): add support for saving all hero data to backend * refactor(LandingPage.tsx): change hero prop to currentHero and remove unnecessary props * refactor(LandingPage.tsx): remove unused state and import statements * refactor(LandingPage.tsx): remove unnecessary console.log statements * refactor(LandingPage.tsx): remove unused code and comments

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -1,40 +1,75 @@
-import { Hero } from "../utils/settings"
-import Footer from "./Footer"
-import HeroAbout from "./HeroAbout"
-import HeroContact from "./HeroContact"
-import HeroCover from "./HeroCover"
+import { useContext, useEffect, useState } from 'react'
+import { HEROES, Hero } from '../utils/settings'
+import Footer from './Footer'
+import HeroAbout from './HeroAbout'
+import HeroContact from './HeroContact'
+import HeroCover from './HeroCover'
+import { useParams } from 'react-router-dom'
+import { UserContext } from '../context/UserContext'
+import { HeroContext } from '../context/HeroContext'
 
-export default function LandingPage({hero}: {hero?: Hero}) {
+export default function LandingPage({ heroes }: { heroes?: Array<Hero> }) {
+  //   const [hero, setHero] = useState<Hero>()
+  const { username } = useParams()
+
+  const userContext = useContext(UserContext)
+  const { currentHero, setCurrentHero } = useContext(HeroContext)
+
+  useEffect(() => {
+    fetch(HEROES)
+      .then((promisse) => promisse.json())
+      .then((heroes: Array<Hero>) =>
+        setCurrentHero(heroes.filter((hero) => hero.username === username)[0])
+      )
+      .catch((error) => console.log(error))
+  }, [])
+
+  function handleSaveAll() {
+    
+    // cover is a file field on backend and we don't want to patch it in this function
+    delete currentHero?.cover
+
+    fetch(`${HEROES}/${currentHero?.id}/`, {
+      method: 'PATCH',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        'Authorization': `Token ${userContext.currentUser?.token}`,
+      },
+      body: JSON.stringify(currentHero),
+    })
+      .then((promisse) => promisse.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error))
+  }
+
   return (
     <>
-      {hero && (
+      {currentHero && (
         <div className='tm-container-fluid'>
-          <HeroCover
-            name={hero.name}
-            cover={
-              hero.cover_url || `$https://ui-avatars.com/api/?name=${hero.name}`
-            }
-          />
-          <HeroAbout about={hero.bio} />
-          <HeroContact
-            instagram={hero.instagram}
-            twitter={hero.twitter}
-            linkedin={hero.linkedin}
-            facebook={hero.facebook}
-            phone={hero.phone}
-            address={hero.address}
-            whatsapp={hero.whatsapp}
-          />
+          <HeroCover />
+          <HeroAbout />
+          <HeroContact />
+          {userContext.currentUser?.username === currentHero.username && (
+            <button onClick={handleSaveAll}>Salvar tudo</button>
+          )}
           <Footer />
         </div>
       )}
       <button
         onClick={(e) => {
           e.preventDefault()
-          console.log(hero)
+          console.log(currentHero)
         }}
       >
         Hero
+      </button>
+      <button
+        onClick={(e) => {
+          e.preventDefault()
+          console.log(userContext.currentUser)
+        }}
+      >
+        CurrentUser
       </button>
     </>
   )
