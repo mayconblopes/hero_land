2b5e85f01a8945dc82e042a1bd4afe7442e8252f
mayconblopes <lopes.maycon@gmail.com>
2023-06-02 22:03:15 -0300

    * feat(LoginForm.tsx): add useParams hook to get username from URL and prefill username input field * feat(LoginForm.tsx): add navigator to redirect to user page after successful login * style(loginForm.css): add basic styling to login form and input fields

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,11 +1,15 @@
 import { useContext, useState } from 'react'
 import { UserContext } from '../context/UserContext'
 import { APIURL, User } from '../utils/settings'
+import './loginForm.css'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export default function LoginForm() {
-  const [user, setUser] = useState<User>({ username: '', password: '' })
+  const {username} = useParams()
+  const [user, setUser] = useState<User>({ username: username, password: '' })
 
   const userContext = useContext(UserContext)
+  const navigator = useNavigate()
 
   function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
     if (e.target.name === 'username') {
@@ -23,38 +27,42 @@ export default function LoginForm() {
 
   function handleLogin(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault()
-    let loggedIn: {username: string, token: string, error?: string} | null = null
+    let loggedIn: { username: string; token: string; error?: string } | null =
+      null
 
     if (user.username && user.password) {
-        fetch(`${APIURL}/login`, {
+      fetch(`${APIURL}/login`, {
         method: 'POST',
         headers: { 'Content-type': 'application/json; charset=UTF-8' },
         body: JSON.stringify(user),
       })
         .then((response) => response.json())
         .then((data) => {
-            console.log(data)
-            loggedIn = data
-            if (loggedIn!.error){
-                console.log(loggedIn!.error)
-            }
-            else {
-                userContext.setCurrentUser(loggedIn)
-            }
+          console.log(data)
+          loggedIn = data
+          if (loggedIn!.error) {
+            console.log(loggedIn!.error)
+          } else {
+            userContext.setCurrentUser(loggedIn)
+            navigator(`/${user.username}`)
+          }
         })
         .catch((error) => console.log('error while login', error))
     }
   }
   return (
-    <form>
+    <form id='login-form'>
       <input
+        id='username-input'
         type='text'
         name='username'
         placeholder='username'
         onChange={handleOnChange}
         value={user.username}
+        disabled
       />
       <input
+        id='password-input'
         type='password'
         name='password'
         placeholder='password'
