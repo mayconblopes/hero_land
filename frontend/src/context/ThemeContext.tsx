03df986f36d42500a25282a142f7e4ededf63fee
mayconblopes <lopes.maycon@gmail.com>
2023-06-04 10:43:26 -0300

    feat(ThemeContext.tsx) *add useEffect hook to fetch themes from server and set current theme based on current hero's theme.

--- frontend/src/context/ThemeContext.tsx
+++ frontend/src/context/ThemeContext.tsx
@@ -1,14 +1,17 @@
-import { createContext, useState } from 'react'
-import { IThemeContext, IUserContext, Theme, User } from '../utils/settings'
+import { createContext, useContext, useEffect, useState } from 'react'
+import { IThemeContext, THEMES, Theme } from '../utils/settings'
+import { HeroContext } from './HeroContext'
 
 interface Props {
   children: React.ReactNode
 }
 
 const defaultTheme: Theme = {
-  coverBGColor: '#F96699',
-  aboutBGColor: '#38CCCC',
-  contactBGColor: '#99CCCC',
+  cover_bgcolor: '',
+  about_bgcolor: '',
+  contact_bgcolor: '',
+  page_bgcolor: '',
+  font_color: '',
 }
 
 export const ThemeContext = createContext<IThemeContext>({
@@ -19,6 +22,29 @@ export const ThemeContext = createContext<IThemeContext>({
 export function ThemeProvider({ children }: Props) {
   const [currentTheme, setCurrentTheme] = useState<Theme>(defaultTheme)
   const context = { currentTheme, setCurrentTheme }
+  const { currentHero } = useContext(HeroContext)
+
+  useEffect(() => {
+    fetch(`${THEMES}`)
+      .then((promisse) => promisse.json())
+      .then((themes) => {
+        const theme = themes.filter(
+          (theme: { id: string }) => theme.id === currentHero?.theme
+        )[0]
+        if (theme) {
+          console.log('THEME', theme)
+          setCurrentTheme({
+            id: theme.id,
+            cover_bgcolor: theme.cover_bgcolor,
+            about_bgcolor: theme.about_bgcolor,
+            contact_bgcolor: theme.contact_bgcolor,
+            page_bgcolor: theme.page_bgcolor,
+            font_color: theme.font_color
+          })
+        }
+      })
+      .catch((error) => console.log(error))
+  }, [currentHero?.theme])
 
   return (
     <ThemeContext.Provider value={context}>{children}</ThemeContext.Provider>
