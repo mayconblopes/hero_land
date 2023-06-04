39469eaba73be8527dcf9098d515d77c33b880fc
mayconblopes <lopes.maycon@gmail.com>
2023-06-04 10:21:42 -0300

    feat(admin.py): set theme as readonly field, because it has to be modified only programmatically.

--- hero_land/admin.py
+++ hero_land/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import HeroModel
+from .models import HeroModel, ThemeModel
+
 
 # Register your models here.
 @admin.register(HeroModel)
 class HeroAdmin(admin.ModelAdmin):
-    readonly_fields = ['cover_id', 'cover_url']
+    readonly_fields = ['cover_id', 'cover_url', 'theme']
+
+admin.site.register(ThemeModel)
