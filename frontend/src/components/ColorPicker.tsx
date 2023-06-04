d739d9c027919e81a3998e45615fbc07fb9aa03a
mayconblopes <lopes.maycon@gmail.com>
2023-06-04 10:34:57 -0300

    ColorPicker UX adjusts * feat(ColorPicker.tsx): add FontAwesomeIcon to ColorPicker button * fix(ColorPicker.tsx): adjust ColorPicker position and add console.log to toggleColorPickerVisibility function * refactor(ColorPicker.tsx): adjust button style and add FECHAR text to close button

--- frontend/src/components/ColorPicker.tsx
+++ frontend/src/components/ColorPicker.tsx
@@ -3,11 +3,15 @@ import { ColorResult } from 'react-color'
 import { ChromePicker } from 'react-color'
 import { ElementToChangeInThemeContext } from '../utils/settings'
 import { ThemeContext } from '../context/ThemeContext'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFillDrip } from '@fortawesome/free-solid-svg-icons'
 
-
-
-export default function ColorPicker({elementToChange}: {elementToChange: ElementToChangeInThemeContext}) {
-    const {currentTheme, setCurrentTheme} = useContext(ThemeContext)
+export default function ColorPicker({
+  elementToChange,
+}: {
+  elementToChange: ElementToChangeInThemeContext
+}) {
+  const { currentTheme, setCurrentTheme } = useContext(ThemeContext)
   const [colorPickerState, setColorPickerState] = useState({
     color: {
       hex: currentTheme[elementToChange],
@@ -15,45 +19,60 @@ export default function ColorPicker({elementToChange}: {elementToChange: Element
     visible: false,
   })
 
-  function toggleColorPickerVisibility(){
+  function toggleColorPickerVisibility() {
     setColorPickerState({
-            ...colorPickerState,
-            visible: !colorPickerState.visible,
-          })
+      ...colorPickerState,
+      visible: !colorPickerState.visible,
+    })
+    console.log('visibilidade')
+    console.log(colorPickerState)
   }
 
-  function handleOnChangeComplete(color: ColorResult){
-    setColorPickerState({...colorPickerState, color: {...color, hex: color.hex}})
-    setCurrentTheme({...currentTheme, [elementToChange]: colorPickerState.color.hex})
+  function handleOnChangeComplete(color: ColorResult) {
+    setColorPickerState({
+      ...colorPickerState,
+      color: { ...color, hex: color.hex },
+    })
+    setCurrentTheme({
+      ...currentTheme,
+      [elementToChange]: colorPickerState.color.hex,
+    })
   }
 
   return (
     <Fragment>
-      <button
-        onClick={toggleColorPickerVisibility}
-      >
-        Color
+      <button onClick={toggleColorPickerVisibility} style={{ padding: '10px' }}>
+        <FontAwesomeIcon icon={faFillDrip} />
       </button>
       {colorPickerState.visible && (
         <div
           style={{
             position: 'absolute',
             zIndex: '2',
-            left: 'auto',
+            left: '20%',
           }}
         >
           <div
             style={{
-              position: 'fixed',
+              position: 'absolute',
               top: 'auto',
               right: 'auto',
               bottom: 'auto',
               left: 'auto',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '5px'
             }}
           >
-            <button onClick={toggleColorPickerVisibility} >X</button>
-            <ChromePicker color={colorPickerState.color.hex} onChangeComplete={handleOnChangeComplete}/>
-          
+            <button onClick={toggleColorPickerVisibility}
+            style={{padding: '10px'}}
+            >
+              FECHAR
+            </button>
+            <ChromePicker
+              color={colorPickerState.color.hex}
+              onChangeComplete={handleOnChangeComplete}
+            />
           </div>
         </div>
       )}
