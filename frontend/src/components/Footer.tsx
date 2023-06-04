88c2f5729db929a305c73a319b0c998160078adb
mayconblopes <lopes.maycon@gmail.com>
2023-06-04 10:27:20 -0300

    * feat(Footer.tsx): add login link to footer and show username in the link if it exists in the URL params * style(Footer.tsx): add space between the HeroLand and the author's name in the footer * chore(Footer.tsx): remove commented code and add target="_blank" to the author's link in the footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,14 +1,21 @@
-export default function Footer(){
-    return (
-        <footer className='tm-footer'>
-          © Todos os direitos reservados
-          {/* <a
-            rel='sponsored'
-            href='https://maycon.barretolopes.com'
-            className='tm-link-gray'
-          >
-            @mayconblopes
-          </a> */}
-        </footer>
-    )
+import { useContext } from "react"
+import { useParams } from "react-router-dom"
+
+export default function Footer() {
+  const {username} = useParams()
+  return (
+    <footer className='tm-footer'>
+      <p>
+        © HeroLand, by{' '}
+        <a
+          rel="sponsored noreferrer"
+          href='https://maycon.barretolopes.com'
+          target="_blank"
+          className='tm-link-gray'
+        >
+          @mayconblopes
+        </a> | <a id='login' href={`/${username}/login`}>login</a>
+      </p>
+    </footer>
+  )
 }
