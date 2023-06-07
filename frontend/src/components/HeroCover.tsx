e56551734f7a36201545eaeea4e036f9e3390ad4
mayconblopes <lopes.maycon@gmail.com>
2023-06-06 21:51:58 -0300

    feat(HeroCover.tsx) * add support for changing hero cover image and updating hero name and skills in real time.

--- frontend/src/components/HeroCover.tsx
+++ frontend/src/components/HeroCover.tsx
@@ -1,4 +1,8 @@
-import { Fragment, useContext } from 'react'
+import {
+  Fragment,
+  useContext,
+  useState,
+} from 'react'
 import { UserContext } from '../context/UserContext'
 import './heroCover.css'
 import { useParams } from 'react-router-dom'
@@ -6,15 +10,31 @@ import { HeroContext } from '../context/HeroContext'
 import ColorPicker from './ColorPicker'
 import { ThemeContext } from '../context/ThemeContext'
 
-export default function HeroCover() {
+type HeroCoverProps = {
+  file?: File
+  setFile: React.Dispatch<React.SetStateAction<File | undefined>>
+}
+
+export default function HeroCover({ file, setFile }: HeroCoverProps) {
   const { username } = useParams()
   const userContext = useContext(UserContext)
   const { currentHero, setCurrentHero } = useContext(HeroContext)
   const { currentTheme } = useContext(ThemeContext)
+  const [heroCover, setHeroCover] = useState(`url(${currentHero?.cover_url})`)
 
-  function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleOnChange(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
     const value = e.target.value
-    setCurrentHero({ ...currentHero, name: value })
+    const key = e.target.name
+    setCurrentHero({ ...currentHero, [key]: value })
+  }
+
+  function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.files) {
+      const file = e.target.files[0]
+      const path = (window.URL || window.webkitURL).createObjectURL(file)
+      setHeroCover(`url(${path})`)
+      setFile(file)
+    }
   }
 
   return (
@@ -27,13 +47,28 @@ export default function HeroCover() {
         style={{
           height: '100%',
           width: '315px',
-          backgroundImage: `url(${currentHero?.cover_url})`,
+          backgroundImage: heroCover,
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           borderRadius: '10px',
         }}
-      />
+      >
+        {userContext.currentUser?.username === username && (
+          <>
+            <label className='file-button' htmlFor='file'>
+              {file?.name || 'Trocar foto...'}
+            </label>
+            <input
+              id='file'
+              type='file'
+              accept='.png, .jpg, .jpeg'
+              onChange={handleFileChange}
+              style={{ display: 'none' }}
+            />
+          </>
+        )}
+      </div>
       {
         // conditional render: current user is the hero owner? (edit enabled)
       }
@@ -43,20 +78,36 @@ export default function HeroCover() {
           <div>
             <ColorPicker elementToChange='cover_bgcolor' />
           </div>
-          
+
           <input
             id='name'
+            name='name'
             value={currentHero?.name}
             onChange={(e) => handleOnChange(e)}
-            style={{marginBottom: '15px'}}
+            style={{ marginBottom: '15px' }}
           />
+           <textarea 
+              name='skills'
+              value={currentHero?.skills}
+              onChange={(e) => handleOnChange(e)}
+            />
         </div>
       ) : (
         <Fragment>
           {
             // conditional render: current user is not the hero owner? (edit disabled)
           }
-          <h1 id='name'>{currentHero?.name}</h1>
+          <section id='cover-container'>
+            <h1 id='name'>{currentHero?.name}</h1>
+            <div id='skills-container'>
+              {currentHero?.skills?.split(',').map((skill) => (
+                <div key={skill} className='skill'>
+                  <p >{skill}</p>
+                </div>
+              ))}
+              <div className='skill-empty'/>
+            </div>
+          </section>
         </Fragment>
       )}
     </section>
